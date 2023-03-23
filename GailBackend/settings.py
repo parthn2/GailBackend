2d9fcd8a2dfdbe3f4872383c25e625bc59f64ab5
Parth Nanda <parthnanda002@gmail.com>
2023-03-23 14:38:55 +0530

    setup for hosting

--- GailBackend/settings.py
+++ GailBackend/settings.py
@@ -11,23 +11,25 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
+from dotenv import load_dotenv
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^9^od$e$2v$%tf6esohdmsdq60e&=wop@wgklfh4x(4@iss@-w'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS = ['gailbackend.onrender.com']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
@@ -78,7 +80,7 @@ WSGI_APPLICATION = 'GailBackend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('postgres://gaildb_user:FM7jv3hw6RrGY7yprBC88Wk2XUDTOGT3@dpg-cgdorfceoogo8r46bg00-a.oregon-postgres.render.com/gaildb').encode(), conn_max_age=600),
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
 
 
