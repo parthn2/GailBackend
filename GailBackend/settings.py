fddddfd60ba3c69cc84c91409407a32c200449bc
Parth Nanda <parthnanda002@gmail.com>
2023-03-23 15:19:57 +0530

    a

--- GailBackend/settings.py
+++ GailBackend/settings.py
@@ -24,10 +24,10 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY ='regfsd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
